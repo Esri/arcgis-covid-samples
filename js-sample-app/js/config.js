f10ffc1a8dcc78c07e0ca770d981178c013c5c88
Andy <agup2001@msn.com>
2020-04-09 09:16:13 -0600

    Add copyright info to .js files

--- js-sample-app/js/config.js
+++ js-sample-app/js/config.js
@@ -1,3 +1,21 @@
+/*
+  Copyright 2020 Esri
+
+  Licensed under the Apache License, Version 2.0 (the "License"); You
+  may not use this file except in compliance with the License. You may
+  obtain a copy of the License at
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+  implied. See the License for the specific language governing
+  permissions and limitations under the License.
+
+  A copy of the license is available in the repository's
+  LICENSE file.
+*/
+
 const layerUrl = "https://services.arcgis.com/DO4gTjwJVIJ7O9Ca/ArcGIS/rest/services/Unacast_Latest_Available__Visitation_and_Distance_/FeatureServer/0";
 const vectorTileLayerID = "1932e7d4432d45dabc0d4d13109c1f09"; // ArcGIS Online Portal ID
 const defaultSliderValues = [-100,40]; // min, max
